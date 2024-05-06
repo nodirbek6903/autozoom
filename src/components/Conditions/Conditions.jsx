6e4c00657999f3ea35abf5f08f05feb00586fa14
NozimjonShukrullayev <nozimshukrullayev@gmail.com>
2024-05-06 15:01:04 +0500

    added translate to conditions and terms section

--- src/components/Conditions/Conditions.jsx
+++ src/components/Conditions/Conditions.jsx
@@ -1,31 +1,35 @@
 import "./Conditions.css"
+import { useTranslation } from "react-i18next";
+
 const Conditions = () => {
+  const { t } = useTranslation();
+
   return (
     <div id="conditions">
       <div className="container">
-        <h3 className="conditions__title">TERMS AND CONDITIONS</h3>
-        <p className="conditions__description">{`The Lessor mentioned overleaf, called Auto Zoom Car Rental, hereby rents vehicle identified overleaf to Renter subject to all terms and conditions on the front page and this page and the Renter, on consideration thereof agrees to them as the essence of the agreement.`}</p>
+        <h3 className="conditions__title">{t("conditions-title")}</h3>
+        <p className="conditions__description">{t("conditions-desc1")}</p>
         <ol>
           <li className="conditions__subtitle">
-            Delivery and Return
+            {t("conditions-subtitle1")}
           </li>
-          <p className="conditions__description">{`The vehicle is delivered to the Renter in good overall condition and without apparent defects. Any complaints as to its condition must be made to the Lessor immediately upon delivery. The Renter agrees to return it with all documents and accessories and in the same condition to the Lessor at the location and on the date designated in this agreement. The Lessor reserves the right to repossess the vehicle without demand at Renter’s expense if vehicle is used in violation of this agreement.`}</p>
+          <p className="conditions__description">{t("conditions-desc2")}</p>
           <li className="conditions__subtitle">
-            Damage, Loss, Theft etc.
+            {t("conditions-subtitle2")}
           </li>
-          <p className="conditions__description">{`In the event of damages or loss of the vehicle or parts of it, including fire and breakage of glass, the Renter shall irrespective of his or the driver’s fault, pay the amount of all resulting loss and expenses to the Lessor (including but not limited to replacement or recovery costs, repair costs, compensation for decline in value and loss of the rental fee). If Standard Terms and Conditions of Rent are violated, any legal provisions (as well as customs regulations or insurance regulations) of loss of revenue the Lessor may charge a compensation corresponding to the rental charge, until the day the vehicle or a replacement vehicle will again be available to the Leas or. This liability of the Renter may be waived if renter accepts Loss Damage Waiver according to Clause 10 by placing his signature in the space “Accept” on the front page, provided that the renter complies in all other respects with the terms and conditions of this Rental Agreement.`}</p>
+          <p className="conditions__description">{t("conditions-desc3")}</p>
           <li className="conditions__subtitle">
-            Charges
+            {t("conditions-subtitle3")}
           </li>
-          <p className="conditions__description">{`Renter shall pay any charges shown on the page or mentioned in the current tariffs. a) The Cash deposit will be returned after 21 working days. b) The Customer has to pay all the expenses (damage, traffic fines, fuel) if the sum of the charges is more than 30% of the deposit.`}</p>
+          <p className="conditions__description">{t("conditions-desc4")}</p>
           <li className="conditions__subtitle">
-            Indemnity
+            {t("conditions-subtitle4")}
           </li>
-          <p className="conditions__description">{`The Lessor is only responsible for loss or damage suffered by Renter or Third Parties, if such loss or damage was caused intentionally or through negligence on Lessor’s part. In all other cases, the Lessor cannot accept any liability and shall be immune against such claims.`}</p>
+          <p className="conditions__description">{t("conditions-desc5")}</p>
           <li className="conditions__subtitle">
-            Conditions of use
+            {t("conditions-subtitle5")}
           </li>
-          <p className="conditions__description">{`– Rented vehicles are not to be driven while Renter or any other driver of vehicle is under the influence of alcohol, hallucinatory drugs, narcotics, barbiturates, or any other substance impairing consciousness or ability to react. Rented vehicles are not to be driven in contravention of any customs, traffic or other regulations;`}</p>
+          <p className="conditions__description">{t("conditions-desc6")}</p>
         </ol>
       </div>
     </div>
