d403aee673b26ec073af6d0aa959cf1435679a47
Abdulloh0901 <lochin.tj@gmail.com>
2024-05-08 02:42:10 -0700

    info carsni xatolik tozaland :)

--- src/components/ParamsCars/paramsmodule/herro-cars.jsx
+++ src/components/ParamsCars/paramsmodule/herro-cars.jsx
@@ -1,128 +1,125 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material";
 
 const HerroCars = () => {
   return (
     <Box sx={{ display: "flex", padding: "10px", gap: "30px" }}>
-    <Stack>
-      <Box sx={{ display: "flex", gap: "10px" }}>
+      <Stack>
+        <Box sx={{ display: "flex", gap: "10px" }}>
+          <Typography
+            sx={{
+              display: "flex",
+              alignItems: "start",
+              color: "white",
+              gap:"5px"
+            }}
+            fontFamily={"Roboto"}
+          >
+            <p
+              style={{
+                display: "flex",
+                fontSize: "26px",
+                lineHeight: "24px",
+                gap:"10px"
+              }}
+            >
+              <span>AED</span>
+              <span> 2000</span>
+            </p>
+            <span
+              style={{
+                display: "flex",
+                fontSize: "22px",
+                lineHeight: "24px",
+                color:"#ffffff85"
+              }}
+            >
+              <span> / $ </span>
+              <span>2000</span>
+            </span>
+          </Typography>
+        </Box>
+        <p style={{ color: "#ffffff7e", margin: "8px 0", fontWeight: "700" }}>
+          per day
+        </p>
         <Typography
           sx={{
-            display: "flex",
-            alignItems: "start",
             color: "white",
-            gap: "35px",
-            width: "198px",
-            justifyContent: "space-between",
-          }}
-          fontFamily={"Roboto"}
-        >
-          <span
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              fontSize: "26px",
-              lineHeight: "24px",
-            }}
-          >
-            {"AED"}
-            <br />
-            {"2000"}
-          </span>
-          <span
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              fontSize: "22px",
-              color: "#ffffff85",
-              lineHeight: "24px",
-            }}
-          >
-            <span>/$</span>
-            <span>2000</span>
-          </span>
-        </Typography>
-      </Box>
-      <p style={{ color: "#ffffff7e", margin: "8px 0", fontWeight: "700" }}>
-        per day
-      </p>
-      <Typography
-        sx={{
-          color: "white",
-          textDecoration: "line-through",
-          fontSize: "18px",
-          fontWeight: "400",
-        }}
-      >
-        <span>AED</span> <span style={{ color: "#ffffff7e" }}>/ $</span>
-      </Typography>
-    </Stack>
-    <Stack sx={{ width: "330px" }} spacing={3}>
-      <Stack direction={"row"} justifyContent={"space-between"}>
-        <Typography
-          sx={{
-            color: "#ffffff7e",
-            fontFamily: "Roboto",
-            fontSize: "13px",
+            textDecoration: "line-through",
+            fontSize: "18px",
+            fontWeight: "400",
           }}
         >
-          Deposit
-        </Typography>
-        <Typography>
-          <span
-            style={{
-              color: "white",
-              fontFamily: "Roboto",
-              fontWeight: "700",
-            }}
-          >
-            AED 1200
-          </span>
-          <p
-            style={{
-              color: "#ffffff7e",
-              fontSize: "12px",
-              fontFamily: "Roboto",
-              fontWeight: "700",
-            }}
-          >
-            AED 1200 for credit cards payment
-          </p>
+          <span>AED</span> <span style={{ color: "#ffffff7e" }}>/ $</span>
         </Typography>
       </Stack>
-      <Stack direction={"row"} justifyContent={"space-between"}>
-        <Typography
-          sx={{
-            color: "#ffffff7e",
-            fontFamily: "Roboto",
-            fontSize: "13px",
-          }}
-        >
-          Premium protection
-        </Typography>
-        <Typography sx={{ transform: "translateX(-58px);" }}>
-          <span
-            style={{
-              color: "white",
-              fontFamily: "Roboto",
-              fontWeight: "700",
-            }}
-          >
-            AED 300
-          </span>
-          <p
-            style={{
+      <Stack sx={{ width: "330px" }} spacing={3}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography
+            sx={{
               color: "#ffffff7e",
-              fontSize: "12px",
               fontFamily: "Roboto",
-              fontWeight: "700",
+              fontSize: "13px",
             }}
           >
-            AED 300 price for 1 day
-          </p>
-        </Typography>
+            Deposit
+          </Typography>
+          <Typography>
+            <span
+              style={{
+                color: "white",
+                fontFamily: "Roboto",
+                fontWeight: "700",
+              }}
+            >
+              AED 1200
+            </span>
+            <p
+              style={{
+                color: "#ffffff7e",
+                fontSize: "12px",
+                fontFamily: "Roboto",
+                fontWeight: "700",
+              }}
+            >
+              AED 1200 for credit cards payment
+            </p>
+          </Typography>
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography
+            sx={{
+              color: "#ffffff7e",
+              fontFamily: "Roboto",
+              fontSize: "13px",
+            }}
+          >
+            Premium protection
+          </Typography>
+          <Typography sx={{ transform: "translateX(-58px);" }}>
+            <span
+              style={{
+                color: "white",
+                fontFamily: "Roboto",
+                fontWeight: "700",
+              }}
+            >
+              AED 300
+            </span>
+            <p
+              style={{
+                color: "#ffffff7e",
+                fontSize: "12px",
+                fontFamily: "Roboto",
+                fontWeight: "700",
+              }}
+            >
+              AED 300 price for 1 day
+            </p>
+          </Typography>
+        </Stack>
       </Stack>
-    </Stack>
-  </Box>  )
-}
+    </Box>
+  );
+};
 
-export default HerroCars
+export default HerroCars;
